d538fb9eb403bcda696d8c0738ff002c6f025aa1
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-07-17 19:12:25 -0500

    wasa

--- src/frontend/components/Navbar/Navbar.jsx
+++ src/frontend/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { LinksContainer } from '..';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './Navbar.module.css';
-import lc2 from "../Footer/images/lc2.png"
+//import lc2 from "../Footer/images/lc2.png"
 
 const Navbar = () => {
   const location = useLocation();
@@ -18,12 +18,12 @@ const Navbar = () => {
         }`}
       >
         <Link to='/' className={styles.logoLink}>
-          <img
-            src={lc2} // Replace with the actual path to your image
+      {/*     <img
+            src={lc2} 
             alt="Logo"
             height="80"
             className={styles.logoImage}
-          />
+          /> */}
          
           <div className={styles.logoText}>
             <h3 className={styles.logo}>
